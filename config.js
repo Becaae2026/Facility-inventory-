88b75194bb294ed4d898c9024ec801e4420acea8
Aneesh <aneeshchandran619@gmail.com>
2026-08-05 14:46:58 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@
 var SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwlqTOFQiNqR07DrlIMuYMWL7ym2gcsuM1T5c87l_8Ei5jZJyCuQrTP_jogplejb-e0Ww/exec";
 
 /* Must match CFG.TOKEN on line 30 of Code.gs, character for character. */
-var API_TOKEN  = "bec-aae-fac-Aa92702689";
+var API_TOKEN  = "bec-aae-fac-Aa92702689";  
 
 /* The sheet itself — only used for the "Open the Google Sheet" link. */
 var SHEET_URL  = "https://docs.google.com/spreadsheets/d/1HhgP1A5qcnc4yD_7ubGpb3gZQlqIE9VHBMhfNtLlVE8/edit";
